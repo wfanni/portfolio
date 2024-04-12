30bc193b8b72d7767108312795341f8047653a89
Fanni Wihl <fanniwihl@gmail.com>
2024-04-12 18:45:00 +0200

    Intro font size changes

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -13,8 +13,8 @@ export default function Intro() {
             <h1 className="relative w-full mt-[6rem] flex flex-col items-center justify-center">
                 {/* <span className="absolute xs:translate-x-[-30px] sm:translate-x-[-30px] md:translate-x-[-100px] lg:translate-x-[-100px] xs:text-4xl sm:text-[4rem] uppercase xs:animate-fadeInLeftXS sm:animate-fadeInLeftS md:animate-fadeInLeft lg:animate-fadeInLeftLG">Hi, I'm Fanni</span>
                 <span className={`absolute ${subtitleOpacity === false ? 'opacity-0' : 'opacity-100'} xs:translate-x-[25px] sm:translate-x-[25px] md:translate-x-[100px] lg:translate-x-[293px] translate-y-[3.5rem] xs:text-xl sm:text-[2.5rem] text-orange uppercase xs:animate-fadeInRightXS sm:animate-fadeInRightS md:animate-fadeInRight lg:animate-fadeInRightLG`}>Frontend Developer</span> */}
-            <span className="absolute xs:text-4xl sm:text-[4rem] xl:translate-x-[-210px] uppercase animate-fadeInLeft xl:animate-fadeInLeftXL">Hi, I'm Fanni</span>
-                <span className={`absolute ${subtitleOpacity === false ? 'opacity-0' : 'opacity-100'} xs:text-xl sm:text-[2.5rem] translate-y-[3.5rem] xl:translate-x-[240px] text-orange uppercase animate-fadeInRight xl:animate-fadeInRightXL`}>Frontend Developer</span>
+            <span className="absolute xs:text-[2.25rem] sm:text-[4rem] xl:translate-x-[-210px] uppercase animate-fadeInLeft xl:animate-fadeInLeftXL">Hi, I'm Fanni</span>
+                <span className={`absolute ${subtitleOpacity === false ? 'opacity-0' : 'opacity-100'} xs:text-[1.25rem] sm:text-[2.5rem] translate-y-[3.5rem] xl:translate-x-[240px] text-orange uppercase animate-fadeInRight xl:animate-fadeInRightXL`}>Frontend Developer</span>
             </h1>
         </section>
     )
