c338f39646dddb4d161e08c39c65912c1f69ce29
Fanni Wihl <fanniwihl@gmail.com>
2024-04-12 18:27:36 +0200

    build folder

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.PNG', "**/*.JPG"],
+  base: './',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: 'build'
+  }
 })
